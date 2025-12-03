bbf1dfd1267f598ce8fb9c8e778ccac197fe3b8d
UserOtt Pessoal <labussework@gmail.com>
2025-12-03 13:49:16 -0300

    Spring Initializr

---
+++ src/test/java/com/readinglis/reading_list_api/ReadingListApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readinglis.reading_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingListApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
